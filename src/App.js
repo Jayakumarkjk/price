fa8e812580fa09bd0e5a08cc259aba5a91d277d0
Jayakumarkjk <jkpdesigns22@gmail.com>
2023-03-02 10:54:57 +0530

    Day 23 Price Cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div class="container py-5">
+    <div className="row text-center align-items-end">         
+              <div className="col-lg-4 mb-5 mb-lg-0">
+                <div className="bg-white p-5 rounded-lg shadow">
+                  <h1 className="h6 text-uppercase font-weight-bold mb-4">FREE</h1>
+                  <h2 className="h1 font-weight-bold">$0<span className="text-small">/month</span></h2>
+                  <hr></hr>
+                  <div className="custom-separator my-4 mx-auto bg-primary"></div>
+                  <ul className="list-unstyled my-5 text-small text-left">
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Single User</li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> 5GB Storage</li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Unlimited Public Projects</li>
+                      <li className="mb-3">
+                      <i className="fa fa-check"></i> Community Access</li>
+                    <li className="mb-3 text-muted">
+                      <i className="fa fa-times mr-2"></i> Unlimited Private Projects </li>
+                    <li className="mb-3 text-muted">
+                      <i className="fa fa-times mr-2"></i> Dedicated Phone Support </li>
+                    <li className="mb-3 text-muted">
+                      <i className="fa fa-times mr-2"></i> Free Subdomain </li>
+                    <li className="mb-3 text-muted">
+                      <i className="fa fa-times mr-2"></i> Monthly Status Reports </li>
+                  </ul>
+                  <a href="#" className="btn btn-primary">BUTTON</a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 mb-5 mb-lg-0">
+                <div className="bg-white p-5 rounded-lg shadow">
+                  <h1 className="h6 text-uppercase font-weight-bold mb-4">PLUS</h1>
+                  <h2 className="h1 font-weight-bold">$9<span className="text-small">/month</span></h2>
+                  <hr></hr>
+                  <div className="custom-separator my-4 mx-auto bg-primary"></div>
+                  <ul className="list-unstyled my-5 text-small text-left">
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> 5 Users</li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> 50GB Storage </li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Unlimited Public Projects</li>
+                      <li className="mb-3">
+                      <i className="fa fa-check"></i> Community Access</li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Unlimited Private Projects </li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Dedicated Phone Support </li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Free Subdomain </li>
+                    <li className="mb-3 text-muted">
+                      <i className="fa fa-times mr-2"></i> Monthly Status Reports </li>
+                  </ul>
+                  <a href="#" className="btn btn-primary">BUTTON</a>
+                </div>
+              </div>
+
+              <div className="col-lg-4 mb-5 mb-lg-0">
+                <div className="bg-white p-5 rounded-lg shadow">
+                  <h1 className="h6 text-uppercase font-weight-bold mb-4">pro</h1>
+                  <h2 className="h1 font-weight-bold">$49<span className="text-small">/month</span></h2>
+                  <hr></hr>
+                  <div className="custom-separator my-4 mx-auto bg-primary"></div>
+                  <ul className="list-unstyled my-5 text-small text-left">
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> unlimited users</li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> 150GB  Storage</li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Unlimited Public Projects</li>
+                      <li className="mb-3">
+                      <i className="fa fa-check"></i> Community Access</li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Unlimited Private Projects </li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Dedicated Phone Support </li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Unlimited free subdomains </li>
+                    <li className="mb-3">
+                      <i className="fa fa-check"></i> Monthly Status Reports </li>
+                  </ul>
+                  <a href="#" className="btn btn-primary">BUTTON</a>
+                </div>
+              </div>
+              </div>
+
+
+           </div>
+   
+ );
 }
 
 export default App;
